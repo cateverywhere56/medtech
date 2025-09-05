308e9f5e2f927ddda0418d6ed1d5802efb460e55
cateverywhere56 <cateverywhere56@gmail.com>
2025-09-05 22:53:23 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://medtech-interventional-news.netlify.app/',
+  site: 'https://hightech-news.netlify.app/',
   build: {
     outDir: 'dist' // <-- on force le dossier de sortie
   },
