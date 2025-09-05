502cf0e99208dfa4b27a5db167a654286239167c
cateverywhere56 <cateverywhere56@gmail.com>
2025-09-05 21:04:39 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://cateverywhere56.github.io/starter',
+  site: 'https://cateverywhere56.github.io/medtech',
   build: {
     outDir: 'dist' // <-- on force le dossier de sortie
   },
